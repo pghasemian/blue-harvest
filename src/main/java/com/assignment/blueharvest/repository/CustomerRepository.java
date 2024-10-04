992ebca1301da8878f391282f4016d54444de86d
pghasemian <ghasemianparisa1995@gmail.com>
2024-10-04 22:33:30 +0200

    added customer entity and repository

---
+++ src/main/java/com/assignment/blueharvest/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.assignment.blueharvest.repository;
+
+import com.assignment.blueharvest.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
+
